d1ec4d4dce50c8c5a61a7deaf3323dd7d3dd3afe
strawfervor <strawfervor@gmail.com>
2025-04-18 13:24:42 +0100

    Added DbContext and files structure.

---
+++ PrzepisyAPI/Controllers/IngredientController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrzepisyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientController : ControllerBase
+    {
+    }
+}
--- PrzepisyAPI/Program.cs
+++ PrzepisyAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PrzepisyAPI.Db;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<RecipeDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
